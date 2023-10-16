3b77a6602278eb581707bcf859f52f3480810141
Jason Gardner <im@jasongardner.co>
2023-10-16 17:51:30 -0500

    fix: Run main without import map

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import tailwindPlugin from "./mod.ts";
-import { join } from "$std/path/mod.ts";
+import { join } from "https://deno.land/std@0.204.0/path/mod.ts";
 
-const css = await Deno.readTextFile(join(Deno.cwd(), "src/style.css"));
+const css = await Deno.readTextFile(join(Deno.cwd(), "src", "style.css"));
 
 const tailwind = tailwindPlugin({
   css,
