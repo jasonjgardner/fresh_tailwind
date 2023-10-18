f01ea4c79ccdf371d799c2f78a43f1cbadcf2d55
Jason Gardner <im@jasongardner.co>
2023-10-18 07:13:36 -0500

    fix: Define deps
    - Fixes #4

---
+++ deps.ts
@@ -0,0 +1,12 @@
+import type {
+  AcceptedPlugin,
+  ProcessOptions,
+  Result,
+} from "https://deno.land/x/postcss@8.4.16/lib/postcss.js";
+import postcss from "https://deno.land/x/postcss@8.4.16/mod.js";
+export { default as autoprefixer } from "https://esm.sh/autoprefixer@10.4.16";
+export { default as tailwindcss } from "npm:tailwindcss@3.3.3";
+
+export type { AcceptedPlugin, ProcessOptions, Result };
+
+export { postcss };
