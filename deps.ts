4b9520e216807a20d929074d2ab81abc142c0531
Jason <im@jasongardner.co>
2023-10-25 11:00:05 -0500

    upd: Added template links to README (#13)
    * upd: Bump Tailwind
    
    * upd: Add template to README.md
    
    - Added template links to README file

--- deps.ts
+++ deps.ts
@@ -5,7 +5,7 @@ import type {
 } from "https://deno.land/x/postcss@8.4.16/lib/postcss.js";
 import postcss from "https://deno.land/x/postcss@8.4.16/mod.js";
 export { default as autoprefixer } from "https://esm.sh/autoprefixer@10.4.16";
-export { type Config, default as tailwindcss } from "npm:tailwindcss@3.3.4";
+export { type Config, default as tailwindcss } from "npm:tailwindcss@3.3.5";
 
 export type { AcceptedPlugin, ProcessOptions, Result };
 
