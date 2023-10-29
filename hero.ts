fbef66cb2f88a22c7af5b60ec5dd5cf825a29025
Jason Gardner <im@jasongardner.co>
2023-10-29 13:10:49 -0500

    upd: Heropattern replacement regex

--- hero.ts
+++ hero.ts
@@ -1,6 +1,6 @@
 export function pattern(color: string, svgContent: string) {
   const svg = encodeURIComponent(
-    svgContent.replace("#000000", color),
+    svgContent.replace(/#[0]{3,6}/, color),
   );
   return `background-image: url('data:image/svg+xml;utf8,${svg}');`;
 }
